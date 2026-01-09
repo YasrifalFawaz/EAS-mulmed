cbf9d5ea5a5edf66ebd9b43aa37252d1b5822892
Yasrifal Fawaz <yasrifalf@gmail.com>
2026-01-10 06:53:50 +0700

    final

--- src/branching/BranchingData.jsx
+++ src/branching/BranchingData.jsx
@@ -1,7 +1,7 @@
 const branchingData = {
   intro: {
-    video: "https://drive.google.com/file/d/1C4QAw0nHw7F1QzcKFjIhRPLTY5i-JzQO/preview",
-    duration:26,
+    video: "https://drive.google.com/file/d/1KZYAvWCJn3zPzOZt0usaGKQ6NBStpzd9/preview",
+    duration:0,
     choices: [
       { text: "Membela", next: "duaA" },
       { text: "Ikut membully", next: "duaB" },
@@ -10,41 +10,36 @@ const branchingData = {
   },
 
   duaA: {
-    video: "https://drive.google.com/file/d/FOREST_ID/preview",
+    video: "https://drive.google.com/file/d/1mGokbd0CarWjmmcQ943PNgVPNLmFR4Wj/preview",
     duration: 0,
     choices: [
-      { text: "Tario Rio Pergi", next: "duaAsatu" },
+      { text: "Tarik Rio Pergi", next: "duaAsatu" },
       { text: "Laporkan ke Satgas", next: "duaAdua" },
-
     ],
   },
 
   duaAsatu: {
-    video: "https://drive.google.com/file/d/CITY_ID/preview",
+    video: "https://drive.google.com/file/d/1rhaPitkst2FqZb71EpWlrDPdqPVJ5wio/preview",
     duration:0,
     choices: [
-      { text: " ", next: "" },
-      { text: "", next: "" },
     ],
   },
 
   duaAdua: {
-    video: "https://drive.google.com/file/d/HOUSE_ID/preview",
+    video: "https://drive.google.com/file/d/1DAui_qVPgpJ91qesAJK2wV3bhmfJwOYD/preview",
     duration:0,
     choices: [],
   },
 
   duaB: {
-    video: "https://drive.google.com/file/d/BUILDING_ID/preview",
+    video: "https://drive.google.com/file/d/1Q-YRh4GRNtsWSU5xV8c9Rh3nUknu9Myw/preview",
     duration:0,
     choices: [
-      { text: "", next: "" },
-      { text: "", next: "" },
     ],
   },
 
   duaC: {
-    video: "https://drive.google.com/file/d/CITY_ID/preview",
+    video: "https://drive.google.com/file/d/1-Y3APVJrdbltpzJE21Khx8dMi7hymsvP/preview",
     duration:0,
     choices: [
       { text: "Mengantar dan Meminta Maaf", next: "duaCsatu" },
@@ -53,13 +48,13 @@ const branchingData = {
   },
 
   duaCsatu: {
-    video: "https://drive.google.com/file/d/HOUSE_ID/preview",
+    video: "https://drive.google.com/file/d/1ygoJ1YZr1-ItOzcHNU6oXZK4SszF3NM_/preview",
     duration:0,
     choices: [],
   },
 
   duaCdua: {
-    video: "https://drive.google.com/file/d/BUILDING_ID/preview",
+    video: "https://drive.google.com/file/d/1HhyyiadAVcSo5gW_HS5fEmg0PMukNQt-/preview",
     duration:0,
     choices: [],
   },
--- src/pages/Trailer.jsx
+++ src/pages/Trailer.jsx
@@ -4,7 +4,7 @@ export default function Trailer() {
   const navigate = useNavigate();
 
   const trailerEmbed =
-    "https://drive.google.com/file/d/1kEYbZcDmhnFDJRVhkIlVmwddyPPKmM_Q/preview";
+    "https://drive.google.com/file/d/1QXHBAGoQuG-Qf_s_z2mn2Wt26SR9ItEg/preview";
 
   return (
     <div className="video-page">
